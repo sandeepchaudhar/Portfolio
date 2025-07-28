cd61bd2b6b840d2d6a11d8ab7e79d7fc13451a5d
Sandy <sandeeppatel979413@gmail.com>
2025-07-28 22:41:30 +0530

    Fix import path for Home component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
  
 import  About  from "./components/About/About";
 import Contact from "./components/Contact/Contact";
-import Home from "./components/Home/home"
+import Home from "./components/Home/Home"
 
 import Nav from "./components/Nav/Nav"
 import Projects from "./components/Projects/Projects";
